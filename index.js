866727b98bdf6b3691bebd9fba5b9dc639ae63be
darshan-psl <darshan.dhanuka@pokersportsleague.com>
2023-10-22 00:06:41 +0530

    third  commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function MainContent(){
     return (
-        <h1 className="header">Hi, This is my first React Project</h1>
+        <div>
+            <h1 className="header">Hi, This is my first React Project</h1>
+            <p>Hi, This is darshan</p>
+        </div>
     )
 }
 
 
-ReactDOM.render(<div>
-                    <MainContent />
-                </div> , 
+const page = (
+    <nav>
+        <h1>First React nav . </h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(page , 
 document.getElementById("root"));
